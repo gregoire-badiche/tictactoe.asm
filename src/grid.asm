84232e42d54de8449bfbe3598cb67a6cff2f318a
Grégoire <puma-cattle-bunch@duck.com>
2025-04-09 14:02:33 +0200

    added coordinates and switch player to grid

--- src/grid.asm
+++ src/grid.asm
@@ -1,29 +1,34 @@
 %ifndef __GRID_ASM__
 %define __GRID_ASM__
 
+%include "src/io.asm"
+
 section .data
 
 characters:
     db 0x20 ; space
     db 0x58 ; X
     db 0x4F ; O
+current_player db 0
 
 grid:
-    db "   |   |   ", 0x0A
-    db "---+---+---", 0x0A
-    db "   |   |   ", 0x0A
-    db "---+---+---", 0x0A
-    db "   |   |   ", 0x0A
+    db "   1   2   3 ", 0x0A ; len 14
+    db "a    |   |   ", 0x0A
+    db "  ---+---+---", 0x0A
+    db "b    |   |   ", 0x0A
+    db "  ---+---+---", 0x0A
+    db "c    |   |   ", 0x0A
 grid_len equ $ - grid
 
 section .text
 
+; Args : x (64 bits reg), y (64 bits reg), v (8 bits reg)
 %macro set_case 3
     push rcx
     mov rcx, %2
     imul rcx, rcx, 3
     add rcx, %1
-    mov [cases + rcx], byte %3
+    mov [cases + rcx], %3
     pop rcx
 %endmacro
 
@@ -37,6 +42,15 @@ section .text
     pop rcx
 %endmacro
 
+play_turn: ; rbx rcx
+    xor rax, rax
+    mov al, [current_player]
+
+    ; switch_players
+    inc al
+    and al, 0x01
+    mov [current_player], al
+    ret
 display_grid:
     push rax
     push rcx
@@ -44,36 +58,32 @@ display_grid:
     push rsi
     xor rax, rax
     mov rcx, 2 ; loop counter
-    xor rdx, rdx
-    xor rsi, rsi
 .set_cases: ; loop + unwinding, setting the grid
+    xor rdx, rdx
     mov rsi, rcx
     mov dl, [cases + rsi]
     mov dl, [characters + rdx]
     imul rax, rcx, 4
-    inc rax
+    add rax, 17
     mov [grid + rax], dl
 
     add rsi, 3
     mov dl, [cases + rsi]
     mov dl, [characters + rdx]
-    add rax, 24
+    add rax, 28
     mov [grid + rax], dl
 
     add rsi, 3
-    mov dl, [cases  + rsi]
+    mov dl, [cases + rsi]
     mov dl, [characters + rdx]
-    add rax, 24
+    add rax, 28
     mov [grid + rax], dl
+
     dec rcx
     jns .set_cases ; jump back (loop)
 
     ; display the grid
-    mov rax, 1 ; syscall: write
-    mov rdi, 1 ; stdout
-    mov rsi, grid ; message
-    mov rdx, grid_len ; len
-    syscall
+    write grid, grid_len
     pop rsi
     pop rdx
     pop rcx
