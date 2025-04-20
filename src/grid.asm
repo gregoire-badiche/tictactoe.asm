c3ca452569940f1d80815ed305ef2dd1d2027c1a
Grégoire <puma-cattle-bunch@duck.com>
2025-04-20 14:58:24 +0200

    added current player indication

--- src/grid.asm
+++ src/grid.asm
@@ -9,7 +9,7 @@ characters:
     db 0x20 ; space
     db 0x58 ; X
     db 0x4F ; O
-current_player db 0
+current_player db 1
 
 grid:
     db "   a   b   c ", 0x0A ; len 14
@@ -19,6 +19,9 @@ grid:
     db "  ---+---+---", 0x0A
     db "3    |   |   ", 0x0A
 grid_len equ $ - grid
+
+message_turn db " 's turn", 0x0A
+message_turn_len equ $ - message_turn
 
 section .text
 
@@ -42,20 +45,22 @@ section .text
     pop rcx
 %endmacro
 
-play_turn: ; rbx rcx
+change_turn: ; rbx rcx
     xor rax, rax
-    mov al, [current_player]
+    mov al, [n_turns]
 
     ; switch_players
     inc al
+    mov [n_turns], al ; increases the number of turns
+
     and al, 0x01
     mov [current_player], al
     ret
 display_grid:
-    push rax
-    push rcx
-    push rdx
-    push rsi
+    ;push rax
+    ;push rcx
+    ;push rdx
+    ;push rsi
     xor rax, rax
     mov rcx, 2 ; loop counter
 .set_cases: ; loop + unwinding, setting the grid
@@ -84,14 +89,21 @@ display_grid:
 
     ; display the grid
     write grid, grid_len
-    pop rsi
-    pop rdx
-    pop rcx
-    pop rax
+    ;pop rsi
+    ;pop rdx
+    ;pop rcx
+    ;pop rax
+display_whos_turn:
+    xor rax, rax
+    mov al, [current_player]
+    mov al, [characters + rax]
+    mov byte [message_turn], al
+    write message_turn, message_turn_len
     ret
 
 section .bss
 ; array of vals
+n_turns resb 1
 cases:
     resb 9
 
--- src/tictactoe.asm
+++ src/tictactoe.asm
@@ -13,13 +13,13 @@ _start: ;
     call display_grid
     call get_input ; rax, rbx now contains the coordinates
     mov dl, [current_player]
-    inc dl
     set_case rax, rbx, dl
+    call change_turn
     call display_grid
     ; jmp _start
 .exit:
-    xor rax, rax ; Set rax to 0
-    mov al, 60 ; set rax to 60 (exit syscall)
+    ; xor rax, rax ; Set rax to 0
+    mov rax, 60 ; set rax to 60 (exit syscall)
     xor rdi, rdi ; set rdi to 0 (exit code 0)
     syscall
 
