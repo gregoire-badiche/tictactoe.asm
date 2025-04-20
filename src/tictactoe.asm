3a455c886f6253dcd03d7d7eb0d5e608f2d08aa2
Grégoire <puma-cattle-bunch@duck.com>
2025-04-20 19:49:00 +0200

    basic tictactoe without bot ended, yaaay

--- src/tictactoe.asm
+++ src/tictactoe.asm
@@ -6,12 +6,10 @@
 
 ; Args : x (64 bits reg), y (64 bits reg), v (8 bits reg)
 %macro set_case 3
-    ; push rcx
     mov rcx, %2
     imul rcx, rcx, 3
     add rcx, %1
     mov [cases + rcx], %3
-    ; pop rcx
 %endmacro
 
 %macro get_case 3
@@ -66,48 +64,27 @@ input_not_valid_text_len equ $ - input_not_valid_text
 case_already_taken_text db "This case is already taken. Please select a free one : "
 case_already_taken_text_len equ $ - case_already_taken_text
 
+tie_text db "That's a tie", 0x0A
+tie_text_len equ $ - tie_text
+
+win_text db "Victory for  !", 0x0A
+win_text_len equ $ - win_text
+
 section .text
 global _start
 
-_start: ;
-    ; --------------------------------
-    ; display grid
-    xor rax, rax
-    mov rcx, 2 ; loop counter
-.set_cases: ; loop + unwinding, setting the grid
-    xor rdx, rdx
-    mov rsi, rcx
-    mov dl, [cases + rsi]
-    mov dl, [characters + rdx]
-    imul rax, rcx, 4
-    add rax, 17
-    mov [grid + rax], dl
-
-    add rsi, 3
-    mov dl, [cases + rsi]
-    mov dl, [characters + rdx]
-    add rax, 28
-    mov [grid + rax], dl
-
-    add rsi, 3
-    mov dl, [cases + rsi]
-    mov dl, [characters + rdx]
-    add rax, 28
-    mov [grid + rax], dl
-
-    dec rcx
-    jns .set_cases ; jump back (loop)
-
+_start:
     ; Achtually display the grid
     write grid, grid_len
-    ; --------------------------------
+
     ; display who's turn and asks for input
     xor rax, rax
     mov al, [current_player]
     mov al, [characters + rax]
     mov byte [get_input_text], al
     write get_input_text, get_input_text_len
-get_input: ; gets the input, valdates it and flushes stdin
+get_input: ; using rax, rbx, rsi, rdi, 
+    ; gets the input, valdates it and flushes stdin
     xor rax, rax
     xor rdi, rdi
     mov rsi, player_selection
@@ -139,31 +116,92 @@ get_input: ; gets the input, valdates it and flushes stdin
     sub bl, 49
     ; yepeee, the input is valid !!
     ; checking if the case is not already used
-    imul rdx, rax, 3
-    add rdx, rbx
+check_case_valid: ; using rax, rbx, rcx, rdx
+    imul rdx, rbx, 3
+    add rdx, rax
     mov cl, [cases + rdx]
     cmp cl, 0
     jne case_already_taken
-    ; if not,
-    ; We change the selected case
+    ; if not, we change the selected case
     mov dl, [current_player]
-    set_case rax, rbx, dl ; TODO : unwind the macro 
+    ; Sets case in the case grid
+    mov rcx, rbx
+    imul rcx, rcx, 3
+    add rcx, rax
+    mov [cases + rcx], dl
+    ; Sets the case in the displayed string
+    inc rax
+    imul rax, rax, 4
+    imul rbx, rbx, 28
+    add rbx, 14
+    add rax, rbx
+    mov dl, [characters + rdx]
+    mov [current_player + rax], dl
+
+    ; Check for a win
+check_win: ; using rax, rbx, rcx
+    ; columns
+    xor rax, rax
+    xor rbx, rbx
+    xor rcx, rcx
+    mov cl, [current_player]
+.loop:
+    ; check rows
+    mov bl, al
+    cmp cl, [cases + rbx]
+    jne .l2
+    inc bl
+    cmp cl, [cases + rbx]
+    jne .l2
+    inc bl
+    cmp cl, [cases + rbx]
+    je win
+    ; won !!
+.l2:
+    ; check columns
+    mov bl, al
+    cmp cl, [cases + rbx]
+    jne .l3
+    add bl, 3
+    cmp cl, [cases + rbx]
+    jne .l3
+    add bl, 3
+    cmp cl, [cases + rbx]
+    je win
+.l3:
+    ; increments counter
+    inc al
+    cmp al, 3
+    jne .loop
+
+    ; diagonal
+    cmp cl, [cases]
+    jne .l4
+    cmp cl, [cases + 4]
+    jne .l4
+    cmp cl, [cases + 8]
+    je win
+.l4:
+    ; antidiagonal
+    cmp cl, [cases + 3]
+    jne end_turn
+    cmp cl, [cases + 5]
+    jne end_turn
+    cmp cl, [cases + 7]
+    je win
+end_turn:
+    ; switch_players and increments turn counter
     xor rax, rax
     mov al, [n_turns]
-
-    ; switch_players
     inc al
+    cmp al, 9
+    je tie
     mov [n_turns], al ; increases the number of turns
 
-    and al, 0x01
-    inc al
+    and al, 0x01 ; we take the parity of the n of turns, 0 = X, 1 = O
+    inc al ; increments as X = 1 and 0 = 2
     mov [current_player], al
     jmp _start
-exit:
-    ; xor rax, rax ; Set rax to 0
-    mov rax, 60 ; set rax to 60 (exit syscall)
-    xor rdi, rdi ; set rdi to 0 (exit code 0)
-    syscall
 
 input_not_valid:
     write input_not_valid_text, input_not_valid_text_len
@@ -171,6 +209,24 @@ input_not_valid:
 case_already_taken:
     write case_already_taken_text, case_already_taken_text_len
     jmp get_input
+
+win: ; player win in cl, or assume current player won
+    write grid, grid_len
+    mov cl, [characters + rcx]
+    mov [win_text + 12], cl
+    write win_text, win_text_len
+    jmp exit
+
+tie:
+    write grid, grid_len
+    write tie_text, tie_text_len
+    jmp exit
+
+exit:
+    ; xor rax, rax ; Set rax to 0
+    mov rax, 60 ; set rax to 60 (exit syscall)
+    xor rdi, rdi ; set rdi to 0 (exit code 0)
+    syscall
 
 section .bss
 player_selection resb 2
